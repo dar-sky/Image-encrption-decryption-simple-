9f73b2fe068050e7be6e3c72ceb2e57b179ad46c
pk-verma890 <84175560+pk-verma890@users.noreply.github.com>
2021-09-11 06:00:32 +0530

    dark
    just add all files

---
+++ encrption.py
@@ -0,0 +1,26 @@
+
+import os
+from os import listdir
+from os.path import isfile, join
+
+
+pat = input("ENTER PATH :: ")
+aa = [f for f in listdir(pat) if isfile(join(pat, f))]
+key = int(input('Enter Key for encryption of Image : '))
+for i in aa:
+    
+    path =pat+"\\"+i
+    try:
+        print(path)
+        fin = open(path, 'rb')
+        image = fin.read()
+        fin.close()
+        image = bytearray(image)
+        for index, values in enumerate(image):
+            image[index] = values ^ key
+        fin = open(path, 'wb')
+        fin.write(image)
+        fin.close()
+        print('Encryption Done...')
+    except Exception:
+        print('Error caught : ', Exception.__name__)
